791918581d0130db874f578505c48dd91f6b713f
BeforeSunshine <lin_yafei@sina.com>
2019-09-17 23:03:51 +0800

    detect single image

--- demo.py
+++ demo.py
@@ -1,8 +1,129 @@
-from utils.yolo import YOLO
+from __future__ import division
+
+from utils.models import *
+from utils.utils import *
+from utils.datasets import *
+
+import os
+import sys
+import time
+import datetime
+import argparse
+
 from PIL import Image
 import numpy as np
-import cv2
-yolo = YOLO()
-image = Image.open('data/images/1.jpg')
-result = yolo.detect_image(image)
-print(result)
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.ticker import NullLocator
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_folder", type=str, default="data/samples/", help="path to dataset")
+    parser.add_argument("--model_def", type=str, default="config/yolov3-custom.cfg", help="path to model definition file")
+    parser.add_argument("--weights_path", type=str, default="checkpoints/yolov3_ckpt_29.pth", help="path to weights file")
+    parser.add_argument("--class_path", type=str, default="data/classes.names", help="path to class label file")
+    parser.add_argument("--conf_thres", type=float, default=0.4, help="object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
+    parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--checkpoint_model", type=str, help="path to checkpoint model")
+    opt = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    os.makedirs("output", exist_ok=True)
+
+    # Set up model
+    model = Darknet(opt.model_def, img_size=opt.img_size).to(device)
+
+    if opt.weights_path.endswith(".weights"):
+        # Load darknet weights
+        model.load_darknet_weights(opt.weights_path)
+    else:
+        # Load checkpoint weights
+        model.load_state_dict(torch.load(opt.weights_path, map_location=lambda storage, loc: storage))
+
+    model.eval()  # Set in evaluation mode
+
+    classes = load_classes(opt.class_path)  # Extracts class labels from file
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    imgs = []  # Stores image paths
+    img_detections = []  # Stores detections for each image index
+
+    print("\nPerforming object detection:")
+
+ # get input image
+    image_name = os.listdir(opt.image_folder)[0]
+    image_path = opt.image_folder + image_name
+    image = Image.open(image_path)
+    image = transform_image(image)
+    image = torch.unsqueeze(image, 0)
+    image = image.to(device, torch.float)
+
+    image = Variable(image.type(Tensor))
+
+    # Get detections
+    with torch.no_grad():
+        detections = model(image)
+        detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)
+
+
+    # Bounding-box colors
+    cmap = plt.get_cmap("tab20b")
+    colors = [cmap(i) for i in np.linspace(0, 1, 20)]
+
+    # Iterate detections and save plot of detections
+    print("\nSaving images:")
+    for detection in detections:
+        # Create plot
+        img = np.array(Image.open(image_path))
+        plt.figure()
+        fig, ax = plt.subplots(1)
+        ax.imshow(img)
+
+        # Draw bounding boxes and labels of detections
+        if detection is not None:
+            # Convert detection from variable to numpy
+            detection = detection.data.cpu().numpy()
+            # Rescale boxes to original image
+            detection = rescale_boxes(detection, opt.img_size, img.shape[:2])
+            color = random.sample(colors, 1)[0]
+            # bbox_colors = random.sample(colors, n_cls_preds)
+            
+            for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
+                print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
+
+                box_w = x2 - x1
+                box_h = y2 - y1
+
+                # color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+                # Create a Rectangle patch
+                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
+                
+                # Add the bbox to the plot
+                ax.add_patch(bbox)
+                # Add label
+                plt.text(
+                    x1,
+                    y1,
+                    s=classes[int(cls_pred)],
+                    color="white",
+                    verticalalignment="top",
+                    bbox={"color": color, "pad": 0},
+                )
+
+        # Save generated image with detections
+        plt.axis("off")
+        plt.gca().xaxis.set_major_locator(NullLocator())
+        plt.gca().yaxis.set_major_locator(NullLocator())
+        plt.savefig(f"output/{image_name}.png", bbox_inches="tight", pad_inches=0.0)
+        plt.close()
